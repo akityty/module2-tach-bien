32a33267694111306707fc08e3a9944e2bca9260
akityty <congnv.9803@gmail.com>
2019-08-10 22:16:50 +0700

    ok

---
+++ src/FizzBuzz.java
@@ -0,0 +1,25 @@
+public class FizzBuzz {
+    public static String fizzBuzz(int number){
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+        if(isFizz && isBuzz)
+            return "FizzBuzz";
+
+        if(isFizz)
+            return "Fizz";
+
+        if(isBuzz)
+            return "Buzz";
+
+        return number + "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(3));
+        System.out.println(fizzBuzz(5));
+        System.out.println(fizzBuzz(6));
+        System.out.println(fizzBuzz(10));
+        System.out.println(fizzBuzz(15));
+        System.out.println(fizzBuzz(30));
+    }
+}
